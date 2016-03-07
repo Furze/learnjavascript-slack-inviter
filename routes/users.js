2c1ae7fd773d1b520ef4b29cdc5da40835d37927
Troy Cornwall <git@troycornwall.net>
2016-03-07 16:18:56 +1300

    Slight error handling + sends message to private channel

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,16 @@ router.post('/', function(req, res, next) {
   if(req.body.hasOwnProperty('email')){
     var SlackClient = require('slack-api-client');
     var slack = new SlackClient(process.env.SLACK_TOKEN);
+    slack.chat.postMessage({
+      'channel': 'registrations',
+      'message': 'Inviting ' + req.body.email
+    });
     slack.api.users.invite({
       email: req.body.email,
       set_active: 'true'
     }, function (err, resTwo) {
-      if (err) { throw err; }
-      res.send(resTwo);
+      if (err) { res.send(err); }
+      res.render(index);
     });
   } else {
     res.statusCode(400).send('respond with a resource');
